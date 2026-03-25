75730c3030b88f054311c6b2824f68bb192626a2
Will White <willavenger12@gmail.com>
2026-03-25 12:57:51 -0500

    Added router script to prevent pages from flashing on load. Updated tsc config files

--- client/public/sideNavBar.js
+++ client/public/sideNavBar.js
@@ -1,6 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var navHTML = "\n        <nav class=\"sideNavBar\">\n      <ul>\n        <a href=\"../dashboard/dashboard.html\" class=\"sideNavLink\">Dashboard</a>\n        <a href=\"../patients/patients.html\" class=\"sideNavLink\">Patients</a>\n        <a href=\"../appointments/appointments.html\" class=\"sideNavLink\">Appointments</a>\n        <a href=\"../lab-results/lab-results.html\" class=\"sideNavLink\">Lab Results</a>\n        <a href=\"../billing/billing.html\" class=\"sideNavLink\">Billing</a>\n        <a href=\"../settings/settings.html\" class=\"sideNavLink\">Settings</a>\n      </ul>\n        <a href=\"../login/login.html\" class=\"sideNavLink\" id=\"logout\">Logout</a>\n    </nav>\n    ";
-    var navBar = document.getElementById('sideNavContainer');
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const navHTML = `
+        <nav class="sideNavBar">
+      <ul>
+        <a href="../dashboard/dashboard.html" class="sideNavLink">Dashboard</a>
+        <a href="../patients/patients.html" class="sideNavLink">Patients</a>
+        <a href="../appointments/appointments.html" class="sideNavLink">Appointments</a>
+        <a href="../lab-results/lab-results.html" class="sideNavLink">Lab Results</a>
+        <a href="../billing/billing.html" class="sideNavLink">Billing</a>
+        <a href="../settings/settings.html" class="sideNavLink">Settings</a>
+      </ul>
+        <a href="../login/login.html" class="sideNavLink" id="logout">Logout</a>
+    </nav>
+    `;
+    const navBar = document.getElementById('sideNavContainer');
     if (navBar) {
         navBar.innerHTML = navHTML;
     }
