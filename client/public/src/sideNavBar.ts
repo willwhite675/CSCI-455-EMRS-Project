964a402891a69a936f7ef3020b8cafa06503bbee
Will White <william.r.white@und.edu>
2026-04-28 10:44:41 -0500

    Deleted lab Results page since it is on the dashboard and the patient page

--- client/public/src/sideNavBar.ts
+++ client/public/src/sideNavBar.ts
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
       <ul>
         <a href="../dashboard/dashboard.html" class="sideNavLink">Dashboard</a>
         <a href="../appointments/appointments.html" class="sideNavLink">Appointments</a>
-        <a href="../lab-results/lab-results.html" class="sideNavLink">Lab Results</a>
         <a href="../billing/billing.html" class="sideNavLink">Billing</a>
         <a href="../settings/settings.html" class="sideNavLink">Settings</a>
       </ul>
@@ -22,7 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
         <a href="../dashboard/dashboard.html" class="sideNavLink">Dashboard</a>
         <a href="../patients/patients.html" class="sideNavLink">Patients</a>
         <a href="../appointments/appointments.html" class="sideNavLink">Appointments</a>
-        <a href="../lab-results/lab-results.html" class="sideNavLink">Lab Results</a>
         <a href="../billing/billing.html" class="sideNavLink">Billing</a>
         <a href="../settings/settings.html" class="sideNavLink">Settings</a>
       </ul>
@@ -39,7 +37,6 @@ document.addEventListener("DOMContentLoaded", () => {
         <a href="../dashboard/dashboard.html" class="sideNavLink">Dashboard</a>
         <a href="../patients/patients.html" class="sideNavLink">Patients</a>
         <a href="../appointments/appointments.html" class="sideNavLink">Appointments</a>
-        <a href="../lab-results/lab-results.html" class="sideNavLink">Lab Results</a>
         <a href="../billing/billing.html" class="sideNavLink">Billing</a>
         <a href="../management-panel/management-panel.html" class="sideNavLink">Management Panel</a>
         <a href="../settings/settings.html" class="sideNavLink">Settings</a>
